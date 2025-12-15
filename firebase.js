846094306d686d325aa506a9cee7f5f850261794
KRSACC <killgg0108@gmail.com>
2025-12-15 14:10:29 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
@@ -11,9 +12,9 @@ const firebaseConfig = {
   appId: "1:90486936988:web:cd88491ebb12c9cdb96459"
 };
 
+// Firebase 초기화
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
+// 사용할 서비스
+export const auth = getAuth(app);
+export const db = getFirestore(app);
